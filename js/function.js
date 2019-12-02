06fcbd10617cb3ec42e68483c3faa40aa152e936
duonghc <duonghc@gmail.com>
2019-12-02 18:57:21 +0700

    Add blog page

---
+++ js/function.js
@@ -0,0 +1,13 @@
+var clickFnc = function(selector){
+    selector.click(function() {
+        var $this = $(this);
+        if (!$this.hasClass("active"))
+        {
+            $this.addClass("active");
+            $this.next().addClass("active");
+        } else {
+            $this.removeClass("active");
+            $this.next().removeClass("active");
+        }
+    });
+};
