7f650c920719f72d5a4af50571fc74775be2ef41
duonghc <duonghc@gmail.com>
2019-12-06 13:46:24 +0700

    Modified responsive

--- js/function.js
+++ js/function.js
@@ -5,9 +5,11 @@ var clickFnc = function(selector){
         if (!$this.hasClass("active"))
         {
             $this.addClass("active");
+            $this.parent().addClass("active");
             $this.next().addClass("active");
         } else {
             $this.removeClass("active");
+            $this.parent().removeClass("active");
             $this.next().removeClass("active");
         }
     });
