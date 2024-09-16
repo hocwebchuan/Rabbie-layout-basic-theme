87a2794a65ae43bb1dd30a117ef4ae97fd727d23
Duong hc <duonghc@gmail.com>
2024-09-16 15:19:59 +0700

    a

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,57 @@
-$(function(){
-    // Menu for small screen.
-    var menu = $(".menu");
-    clickFnc(menu);
+$(function () {
+  // Menu for small screen.
+  var menu = $(".menu");
+  clickFnc(menu);
 });
+function sweetalertConfirm() {
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "Deleted!",
+        text: "Your file has been deleted.",
+        icon: "success",
+      });
+    }
+  });
+}
+function sweetalertSuccess() {
+  Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "Your work has been saved",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+}
+function sweetalertCountLess() {
+  let timerInterval;
+  Swal.fire({
+    title: "Auto close alert!",
+    html: "I will close in <b></b> milliseconds.",
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const timer = Swal.getPopup().querySelector("b");
+      timerInterval = setInterval(() => {
+        timer.textContent = `${Swal.getTimerLeft()}`;
+      }, 100);
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
+    },
+  }).then((result) => {
+    /* Read more about handling dismissals below */
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log("I was closed by the timer");
+    }
+  });
+}
